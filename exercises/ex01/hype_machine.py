532b389400e80d4f342f430e8d8e0cff97fdd5fd
Luke Chambers <luke360@live.unc.edu>
2021-08-31 14:37:41 -0400

    halfway with numeric operators

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,11 +1,11 @@
-# TODO: Write docstring here ""hype_machine"""
+# TODO: Write docstring here ""Hype Machine exercise."""
 
 # TODO: Initialize __author__ variable here = "730288863"
 
 # TODO: Implement your program logic here 
-name:str = input("What is your name? ")
+name: str = input("What is your name? ")
 print("You entered: ")
 print(name)
-print:str = input("That's exactly right, you are a survivor ")
-print:str = input("I am proud of you Luke, stay winning!! ")
-print:str = input("Keep up the good work and you will be sucessful Luke!")
+print: str = input("That's exactly right, you are a survivor ")
+print: str = input("I am proud of you Luke, stay winning!! ")
+print: str = input("Keep up the good work and you will be sucessful Luke!")
---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,6 @@
+# TODO: Write docstring here ""Numeric Operators exercise."""
+__author__ = "730288863"
+
+name: str = input("Left-hand side ")
+
+name: str
