86feeb3af579025d4cbf448e09206488e7dd69aa
Luke Chambers <luke360@live.unc.edu>
2021-08-31 13:20:21 -0400

    finished hype machine

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,11 @@
-# TODO: Write docstring here
+# TODO: Write docstring here ""hype_machine"""
 
-# TODO: Initialize __author__ variable here
+# TODO: Initialize __author__ variable here = "730288863"
 
-# TODO: Implement your program logic here
+# TODO: Implement your program logic here 
+name:str = input("What is your name? ")
+print("You entered: ")
+print(name)
+print:str = input("That's exactly right, you are a survivor ")
+print:str = input("I am proud of you Luke, stay winning!! ")
+print:str = input("Keep up the good work and you will be sucessful Luke!")
