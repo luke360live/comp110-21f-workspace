36895cb7a2e1675f1d2c21c691459379dd051ad6
Luke Chambers <luke360@live.unc.edu>
2021-09-06 17:48:04 -0400

    halfway ex02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,13 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730288863"
 
 
 # Begin your solution here...
+
+i: int = 0
+search: str = input("What letter do you want to search for?: ")
+word: str = input("Enter a word: ")
+while i < len(word): 
+    how_many: str = str(word.count(search))
+    print("Count: " + how_many)
--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730288863"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,25 @@ from random import randint
 
 
 # Begin your solution here...
+
+
+fortune_one: str = "You will meet the person of your dreams soon."
+fortune_two: str = "Be careful of those who are only friendly in private."
+fortune_three: str = "Love is the only solution to conflict"
+fortune_four: str = "Enjoy each moment and be present"
+str = input("Your fortune cookie says... ")
+choose: int = randint(1, 4)
+if choose == 1:
+    print(fortune_one)
+    print("Now, go spread positive vibes!")
+else:
+    if choose == 2:
+        print(fortune_two)
+        print("Now, go spread positive vibes!")
+    else:
+        if choose == 3: 
+            print(fortune_three)
+            print("Now, go spread positive vibes!")
+        else:
+            print(fortune_four)
+            print("Now, go spread positive vibes!")
