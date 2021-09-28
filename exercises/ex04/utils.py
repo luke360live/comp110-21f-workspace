7959a24fc36725ec8cc04ef901528f41baf61582
Luke Chambers <luke360@live.unc.edu>
2021-09-27 22:00:41 -0400

    draft

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,53 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730288863"
 
 
 # TODO: Implement your functions here.
+
+"""all"""
+
+
+def all(a: list[int], b: int) -> bool:
+    z = 0
+    while z < len(a):
+        number: int = a[z]
+        if b != number or a == list():
+            return False
+        z = z + 1
+    return True 
+
+
+"""is_equal"""
+
+
+def is_equal(x: list[int], y: list[int]) -> bool:
+    z = 0
+    while z < len(x):
+        list_x: int = x[z]
+        list_y: int = y[z]
+        if len(x) != len(y) or list_x != list_y:
+            return False
+        z = z + 1
+    return True
+
+
+"""max"""
+
+
+def max(input: list) -> int:
+    if input == list():
+        raise ValueError
+    z = 0
+    biggest: int = 100
+    while z < len(input):
+        if input[z] > biggest:
+            biggest = input[z]
+        z = z + 1
+    return biggest
+
+
+    
+
+    
+    
