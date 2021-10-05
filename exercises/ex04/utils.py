cbd530d8cdc3ebc81f24ab54491d39eb5056db12
Luke Chambers <luke360@live.unc.edu>
2021-10-05 18:15:54 -0400

    ex04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -9,10 +9,12 @@ __author__ = "730288863"
 
 
 def all(a: list[int], b: int) -> bool:
-    z = 0
+    z = 0 
+    if (len(a) == 0):
+        return False
     while z < len(a):
         number: int = a[z]
-        if b != number or a == list():
+        if b != number:
             return False
         z = z + 1
     return True 
@@ -23,7 +25,14 @@ def all(a: list[int], b: int) -> bool:
 
 def is_equal(x: list[int], y: list[int]) -> bool:
     z = 0
+    if (len(x) == 0 and len(y) == 0):
+        return True
+    if (len(x) == 0):
+        return False
     while z < len(x):
+        if (len(y) == 0):
+            return False
+
         list_x: int = x[z]
         list_y: int = y[z]
         if len(x) != len(y) or list_x != list_y:
@@ -39,7 +48,7 @@ def max(input: list) -> int:
     if input == list():
         raise ValueError
     z = 0
-    biggest: int = 100
+    biggest: int = -999
     while z < len(input):
         if input[z] > biggest:
             biggest = input[z]
